9ed59b721a9b1174dfcb899941f7e80f4bc48ee2
RATHNAPUR BHASKAR <70761644+RBhaskar19@users.noreply.github.com>
2022-04-02 21:46:03 +0530

    Update OrderRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/OrderRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/OrderRepository.java
@@ -1,18 +1,1 @@
-package com.examly.springapp.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.*;
-
-import com.examly.springapp.model.OrderModel;
-
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderModel, String>{
-
-    @Query(value = "SELECT * FROM order WHERE user_id=?1", nativeQuery = true)
-    List<OrderModel> findByUserId(String userId);
-
-
-}
