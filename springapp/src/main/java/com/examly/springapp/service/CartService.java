233a8a63602a04f68b53bfc694dd271ab423e60a
RATHNAPUR BHASKAR <70761644+RBhaskar19@users.noreply.github.com>
2022-04-03 11:27:16 +0530

    Update CartService.java

--- springapp/src/main/java/com/examly/springapp/service/CartService.java
+++ springapp/src/main/java/com/examly/springapp/service/CartService.java
@@ -1,1 +1,80 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.dto.cart.AddToCartDto;
+import com.examly.springapp.dto.cart.CartDto;
+import com.examly.springapp.dto.cart.CartItemDto;
+import com.examly.springapp.exceptions.CartItemNotExistException;
+import com.examly.springapp.model.*;
+import com.examly.springapp.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional
+public class CartService {
+
+    @Autowired
+    private  CartRepository cartRepository;
+
+    public CartService(){}
+
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public void addToCart(AddToCartDto addToCartDto, ProductModel product, UserModel user){
+        Cart cart = new Cart(product, addToCartDto.getQuantity(), user);
+        cartRepository.save(cart);
+    }
+
+
+    public CartDto listCartItems(UserModel user) {
+        List<Cart> cartList = cartRepository.findAllByUserOrderByCreatedDateDesc(user);
+        List<CartItemDto> cartItems = new ArrayList<>();
+        for (Cart cart:cartList){
+            CartItemDto cartItemDto = getDtoFromCart(cart);
+            cartItems.add(cartItemDto);
+        }
+        double totalCost = 0;
+        for (CartItemDto cartItemDto :cartItems){
+            totalCost += (cartItemDto.getProduct().getPrice()* cartItemDto.getQuantity());
+        }
+        return new CartDto(cartItems,totalCost);
+    }
+
+
+    public static CartItemDto getDtoFromCart(Cart cart) {
+        return new CartItemDto(cart);
+    }
+
+
+    public void updateCartItem(AddToCartDto cartDto){
+        Cart cart = cartRepository.getOne(cartDto.getId());
+        cart.setQuantity(cartDto.getQuantity());
+        cart.setCreatedDate(new Date());
+        cartRepository.save(cart);
+    }
+
+    public void deleteCartItem(int id) throws CartItemNotExistException {
+        if (!cartRepository.existsById(id))
+            throw new CartItemNotExistException("Cart id is invalid : " + id);
+        cartRepository.deleteById(id);
+
+    }
+
+    public void deleteCartItems() {
+        cartRepository.deleteAll();
+    }
+
+
+    public void deleteUserCartItems(UserModel user) {
+        cartRepository.deleteByUser(user);
+    }
+}
+
 
