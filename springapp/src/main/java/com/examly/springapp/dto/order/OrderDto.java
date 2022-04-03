b0ae00b02f7f8038b4d86b3c1469a660215b5125
RATHNAPUR BHASKAR <70761644+RBhaskar19@users.noreply.github.com>
2022-04-03 11:19:36 +0530

    Add files via upload

---
+++ springapp/src/main/java/com/examly/springapp/dto/order/OrderDto.java
@@ -0,0 +1,33 @@
+package com.examly.springapp.dto.order;
+
+import com.examly.springapp.model.Order;
+import javax.validation.constraints.NotNull;
+
+public class OrderDto {
+    private Integer id;
+    private @NotNull Integer userId;
+
+    public OrderDto() {
+    }
+
+    public OrderDto(Order order) {
+        this.setId(order.getId());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+}
