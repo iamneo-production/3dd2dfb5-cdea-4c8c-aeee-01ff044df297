366e9aeb63a680e2624bcbc03c7839b360eaf3c5
RATHNAPUR BHASKAR <70761644+RBhaskar19@users.noreply.github.com>
2022-04-03 11:24:39 +0530

    Add files via upload

---
+++ springapp/src/main/java/com/examly/springapp/service/OrderItemsService.java
@@ -0,0 +1,22 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.model.OrderItem;
+import com.examly.springapp.repository.OrderItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class OrderItemsService {
+
+    @Autowired
+    private OrderItemsRepository orderItemsRepository;
+
+    public void addOrderedProducts(OrderItem orderItem) {
+        orderItemsRepository.save(orderItem);
+    }
+
+
+}
