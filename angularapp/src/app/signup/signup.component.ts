a64ddf8ebdf82fd19cee884f127afa87fc91ca2d
Jayaprakash <52954080+jayaprakash1207@users.noreply.github.com>
2022-04-07 21:06:27 +0530

    Update signup.component.ts

--- angularapp/src/app/signup/signup.component.ts
+++ angularapp/src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { SignupService } from '../services/User/signup.service';
+import { SignupService } from '../service/Signup/signup.service';
 
 @Component({
   selector: 'app-signup',
